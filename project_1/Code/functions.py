4fb9ac594efdd48b6234f699099455f3d0f4d5d0
Manuela Nader <manuelalealnader@gmail.com>
2025-09-29 18:14:28 +0200

    Fixed a few things

--- project_1/Code/functions.py
+++ project_1/Code/functions.py
@@ -2,4 +2,4 @@ def runge(x_values):
     """ Arguments: x_values, or a single x_value
     returns: the runge function of the x-values"""
 
-    return 1 / (1 + 25* x_values**2)
+    return 1 / (1 + 25* (x_values**2))
