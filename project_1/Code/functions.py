ce106028b2e165412a0e2c3b8e799f20158d0c9d
Manuela Nader <manuelalealnader@gmail.com>
2025-09-29 21:53:09 +0200

    Started GD

--- project_1/Code/functions.py
+++ project_1/Code/functions.py
@@ -10,7 +10,6 @@ def OLS_parameters(X, y):
     return (np.linalg.pinv(X.T @ X) @ X.T ) @ y
 
 def Ridge_parameters(X, y, lmbda):
-    # Assumes X is scaled and has no intercept column
     return np.linalg.inv(X.T @ X + lmbda * np.eye(X.shape[1])) @ X.T @ y
 
 def polynomial_features(x, p):
@@ -28,3 +27,25 @@ def R2(y_data, y_model):
 def MSE(y_data, y_model):   
     # from week 34 lecture notes
     return np.mean((y_data - y_model) ** 2)
+
+def OLS_gradient_Manuela(X, y, theta):
+    n = y.shape[0] 
+    residuals = y - X @ theta  
+    gradient = -2/n * residuals.T @ X  
+    return gradient
+
+def Ridge_gradient_Manuela(X, y, theta, lmbda):
+    n = y.shape[0]  
+    residuals = y - X @ theta  # Calculate residuals
+    gradient = (-2/n * X.T @ residuals) + (2 * lmbda * theta)  
+    return gradient
+
+def OLS_gradient_Helene(X, y, theta):
+    n = y.shape[0]   
+    gradient = 2.0/n * (X.T @ X @ theta - X.T @ y)
+    return gradient
+
+def Ridge_gradient_Helene(X, y, theta, lam):
+    n = y.shape[0]   
+    gradient = 2.0/n * (X.T @ X @ theta - X.T @ y) + 2*lam*theta
+    return gradient
