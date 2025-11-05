f0684cc8a1dffdfb0344e94fa24de09c7fe5ed66
Manuela Nader <manuelalealnader@gmail.com>
2025-11-05 14:33:39 +0100

    Leaky ReLU and plain GD with ADAM and RMSProp

--- project_2/Code/functions.py
+++ project_2/Code/functions.py
@@ -1,10 +1,14 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
 
+# Runge function
+
 def runge(x_values):
     """ Arguments: x_values, or a single x_value
     returns: the runge function of the x-values"""
     return 1 / (1 + 25* (x_values**2))
+
+# Ordinary Least Squares and Ridge Regression parameter estimations
 
 def OLS_parameters(X, y):
     return (np.linalg.pinv(X.T @ X) @ X.T ) @ y
@@ -20,20 +24,6 @@ def polynomial_features(x, p):
         X[:, i] = x**i
     return X
 
-def identity(x):
-    return x
-
-def identity_der(x):
-    return np.array([1]*len(x))
-
-def R2(y_data, y_model):
-    # from week 34 lecture notes
-    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_model)) ** 2)
-
-def MSE(y_data, y_model):   
-    # from week 34 lecture notes
-    return np.mean((y_data - y_model) ** 2)
-
 def OLS_gradient(X, y, theta):
     n = y.shape[0]   
     gradient = 2.0/n * (X.T @ X @ theta - X.T @ y)
@@ -44,38 +34,37 @@ def Ridge_gradient(X, y, theta, lam):
     gradient = 2.0/n * (X.T @ (X @ theta) - X.T @ y) + 2*lam*theta
     return gradient
 
-def Lasso_gradient(X, y, theta, lam):
-    n = y.shape[0]
-    gradient = 2.0/n * (X.T @ (X @ theta) - X.T @ y) + lam * np.sign(theta)
-    return gradient
+# Activation functions and their derivatives
 
-# Defining some activation functions
+def identity(x):
+    return x
+
+def identity_der(x):
+    return np.array([1]*len(x))
 
 def ReLU(z):
-    # from week 42 exercises
     return np.where(z > 0, z, 0)
 
-
-def sigmoid(z):
-    # from week 42 exercises
-    return 1 / (1 + np.exp(-z))
-
-# Derivative of the ReLU function
 def ReLU_der(z):
-    # from week 42 exercises
     return np.where(z > 0, 1, 0)
 
-# Derivative of sigmoid
+def leaky_ReLU(z):
+    return np.where(z > 0, z, 0.01 * z)
+
+def leaky_ReLU_der(z):
+    return np.where(z > 0, 1, 0.01)
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
 def sigmoid_der(z):
     return sigmoid(z) * (1 - sigmoid(z))
-
 
 def softmax(z):
     """Compute softmax values for each set of scores in the rows of the matrix z.
     Used with batched input data."""
     e_z = np.exp(z - np.max(z, axis=0))
     return e_z / np.sum(e_z, axis=1)[:, np.newaxis]
-
 
 def softmax_vec(z):
     """Compute softmax values for each set of scores in the vector z.
@@ -87,11 +76,13 @@ def softmax_der(z, softmax=softmax):
     s = softmax(z)
     return s * (1 - s)
 
+# Loss functions and their derivatives
+
+def MSE(y_data, y_model):   
+    return np.mean((y_data - y_model) ** 2)
 
 def mse_der(predict, target):
     return 2 * (predict - target) / target.shape[0]
-
-
 
 def cross_entropy(predict, target):
     return np.sum(-target * np.log(predict))
@@ -99,9 +90,10 @@ def cross_entropy(predict, target):
 def cross_entropy_der(predict, target):
     return - (target / predict) / target.shape[0]
 
-def cost(input, layers, activation_funcs, target):
-    predict = feed_forward_batch(input, layers, activation_funcs)
-    return cross_entropy(predict, target)
+# Performance metrics
+
+def R2(y_data, y_model):
+    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_model)) ** 2)
 
 def accuracy(predictions, targets):
     one_hot_predictions = np.zeros(predictions.shape)
@@ -109,233 +101,3 @@ def accuracy(predictions, targets):
     for i, prediction in enumerate(predictions):
         one_hot_predictions[i, np.argmax(prediction)] = 1
     return accuracy_score(one_hot_predictions, targets)
-
-# neural network functions
-
-# def create_layers(network_input_size, layer_output_sizes):
-#     """Creates layers batched"""
-#     layers = []
-
-#     i_size = network_input_size
-#     for layer_output_size in layer_output_sizes:
-#         W = np.random.randn(i_size, layer_output_size)
-#         b = np.zeros(layer_output_size) + 0.01
-#         layers.append((W, b))
-
-#         i_size = layer_output_size
-#     return layers
-
-# def feed_forward_batch(inputs, layers, activation_funcs):
-#     """Feeds forward batched"""
-#     a = inputs
-#     for (W, b), activation_func in zip(layers, activation_funcs):
-#         z = np.dot(a, W) + b
-#         a = activation_func(z)
-#     return a
-
-# def feed_forward_saver(inputs, layers, activation_funcs):
-#     layer_inputs = []
-#     zs = []
-#     a = inputs
-#     for (W, b), activation_func in zip(layers, activation_funcs):
-#         layer_inputs.append(a)
-#         z = np.dot(a, W) + b
-#         a = activation_func(z)
-
-#         zs.append(z)
-
-#     return layer_inputs, zs, a
-
-# def backpropagation(
-#     inputs, layers, activation_funcs, targets, activation_ders, cost_der=mse_der
-# ):
-#     layer_inputs, zs, predicts = feed_forward_saver(inputs, layers, activation_funcs)
-
-#     layer_grads = [() for layer in layers]
-
-#     # We loop over the layers, from the last to the first
-#     for i in reversed(range(len(layers))):
-#         layer_input, z, activation_der = layer_inputs[i], zs[i], activation_ders[i]
-
-#         if i == len(layers) - 1:
-#             # For last layer we use cost derivative as dC_da(L) can be computed directly
-#             dC_da = cost_der(predicts, targets)
-#         else:
-#             # For other layers we build on previous z derivative, as dC_da(i) = dC_dz(i+1) * dz(i+1)_da(i)
-#             print('No last layer')
-#             (W, b) = layers[i + 1]
-#             dC_da = W @ dC_dz.T
-
-#         dC_dz = dC_da.T * activation_der(z)
-#         dC_dW = dC_dz.T @ layer_input
-#         dC_db = dC_dz
-
-#         layer_grads[i] = (dC_dW, dC_db)
-
-#     return layer_grads
-
-# def backpropagation_cross(
-#     inputs, layers, activation_funcs, targets, activation_ders
-# ):
-#     # backpropagation with simplification for softmax + cross-entropy
-#     layer_inputs, zs, predicts = feed_forward_saver(inputs, layers, activation_funcs)
-
-#     layer_grads = [() for layer in layers]
-
-#     # We loop over the layers, from the last to the first
-#     for i in reversed(range(len(layers))):
-#         layer_input, z, activation_der = layer_inputs[i], zs[i], activation_ders[i]
-#         if i == len(layers) - 1:
-#             dC_dz = predicts - targets  # Simplification for softmax + cross-entropy
-#         else:
-#             (W, b) = layers[i + 1]
-#             dC_da = W @ dC_dz.T
-#             dC_dz = activation_der(z) * dC_da.T 
-#         dC_dW = layer_input.T @ dC_dz
-#         dC_db = np.mean(dC_dz, axis=0)
-
-#         layer_grads[i] = (dC_dW, dC_db)
-
-#     return layer_grads
-
-
-# def train_network(
-#     inputs, layers, activation_funcs, activation_ders, targets, cost_der, learning_rate=0.001, epochs=100, momentum=0.3, minibatch_size=15
-# ):
-#     change = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in layers ]
-#     n_data = inputs.shape[0]
-#     m = int(n_data / minibatch_size)
-#     for i in range(epochs):
-#         indices = np.random.permutation(n_data)
-#         x_shuffled = inputs[indices]
-#         y_shuffled = targets[indices]
-#         for i in range(m):
-#             xi = x_shuffled[i : i + minibatch_size]
-#             yi = y_shuffled[i : i + minibatch_size]
-#             layer_grads = backpropagation(xi, layers, activation_funcs, yi, activation_ders, cost_der)
-#             for idx, ((W, b), (W_g, b_g), (W_c, b_c)) in enumerate(zip(layers, layer_grads, change)):
-#                 new_change_W = learning_rate * W_g + momentum * W_c
-#                 new_change_b = learning_rate * b_g + momentum * b_c
-#                 W -= new_change_W[idx]
-#                 b -= new_change_b[idx]
-#                 layers[idx] = (W, b)
-#                 change[idx] = (new_change_W, new_change_b) 
-
-
-class NeuralNetwork:
-    def __init__(
-        self,
-        x_data,
-        y_data,
-        layer_output_sizes,
-        activation_funcs,
-        activation_ders,
-        cost_fun,
-        cost_der,
-    ):
-        self.x_data = x_data
-        self.y_data = y_data
-        self.network_input_size = x_data.shape[1]
-        self.layer_output_sizes = layer_output_sizes
-        self.activation_funcs = activation_funcs
-        self.activation_ders = activation_ders
-        self.cost_fun = cost_fun
-        self.cost_der = cost_der
-        self.layers = self.create_layers()
-
-    def create_layers(self):
-        layers = []
-
-        i_size = self.network_input_size
-        for layer_output_size in self.layer_output_sizes:
-            W = np.random.randn(i_size, layer_output_size)
-            b = np.zeros(layer_output_size) + 0.01
-            layers.append((W, b))
-
-            i_size = layer_output_size
-        return layers
-
-    def predict(self, inputs=None):
-        a = self.x_data if inputs is None else inputs
-        for (W, b), activation_func in zip(self.layers, self.activation_funcs):
-            z = np.dot(a, W) + b
-            a = activation_func(z)
-        return a
-
-    def cost(self):
-        predicts = self.predict()
-        return self.cost_fun(predicts, self.y_data)
-
-    def _feed_forward_saver(self, inputs=None):
-        layer_inputs = []
-        zs = []
-        a = self.x_data if inputs is None else inputs
-        for (W, b), activation_func in zip(self.layers, self.activation_funcs):
-            layer_inputs.append(a)
-            z = np.dot(a, W) + b
-            a = activation_func(z)
-            zs.append(z)
-
-        return layer_inputs, zs, a
-
-    def backpropagation(self, inputs=None, targets=None):
-        inputs = self.x_data if inputs is None else inputs
-        targets = self.y_data if targets is None else targets
-
-        layer_inputs, zs, predicts = self._feed_forward_saver(inputs)
-        layer_grads = [() for _ in self.layers]
-
-        # We loop over the layers, from the last to the first
-        for i in reversed(range(len(self.layers))):
-            layer_input = layer_inputs[i]
-            z = zs[i]
-            activation_der = self.activation_ders[i]
-
-            if i == len(self.layers) - 1:
-                # For last layer we use cost derivative as dC_da(L) can be computed directly
-                dC_da = self.cost_der(predicts, targets)
-                # elementwise multiply by activation derivative
-                dC_dz = (activation_der(z) * dC_da.T).T
-            else:
-                # For other layers we build on previous z derivative: dC_da_prev = dC_dz @ W_next.T
-                next_W, _ = self.layers[i + 1]
-                dC_da = next_W @ dC_dz.T
-                dC_dz = activation_der(z) * dC_da.T
-
-            dC_dW = layer_input.T @ dC_dz
-            # return bias gradient as 1D array (matches stored b shape)
-            dC_db = np.mean(dC_dz, axis=0)
-
-            layer_grads[i] = (dC_dW, dC_db)
-
-        return layer_grads
-
-    def update_weights(self, layer_grads, learning_rate=0.001, momentum=0.0):
-        for idx, ((W, b), (W_g, b_g)) in enumerate(zip(self.layers, layer_grads)):
-            W = W - learning_rate * W_g
-            b = b - learning_rate * b_g
-            self.layers[idx] = (W, b)
-
-    def train_network(
-        self, learning_rate=0.001, epochs=100, momentum=0.3, minibatch_size=1
-    ):
-        change = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in self.layers ]
-        n_data = self.x_data.shape[0]
-        m = int(n_data / minibatch_size)
-        for epoch in range(epochs):
-            indices = np.random.permutation(n_data)
-            x_shuffled = self.x_data[indices]
-            y_shuffled = self.y_data[indices]
-            for batch_idx in range(m):
-                start = batch_idx * minibatch_size
-                end = start + minibatch_size
-                xi = x_shuffled[start:end]
-                yi = y_shuffled[start:end]
-                layer_grads = self.backpropagation(xi, yi)
-                for idx, ((W, b), (W_g, b_g), (W_c, b_c)) in enumerate(zip(self.layers, layer_grads, change)):
-                    new_change_W = learning_rate * W_g + momentum * W_c
-                    new_change_b = learning_rate * b_g + momentum * b_c
-                    W -= new_change_W
-                    b -= new_change_b
-                    self.layers[idx] = (W, b)
-                    change[idx] = (new_change_W, new_change_b) 
---
+++ project_2/Code/nn_class.py
@@ -0,0 +1,174 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(
+        self,
+        x_data,
+        y_data,
+        layer_output_sizes,
+        activation_funcs,
+        activation_ders,
+        cost_fun,
+        cost_der,
+        L1=False,
+        L2=False,
+    ):
+        self.x_data = x_data
+        self.y_data = y_data
+        self.network_input_size = x_data.shape[1]
+        self.layer_output_sizes = layer_output_sizes
+        self.activation_funcs = activation_funcs
+        self.activation_ders = activation_ders
+        self.cost_fun = cost_fun
+        self.cost_der = cost_der
+        self.layers = self.create_layers()
+        self.L1 = L1
+        self.L2 = L2
+
+    def create_layers(self):
+        layers = []
+
+        i_size = self.network_input_size
+        for layer_output_size in self.layer_output_sizes:
+            W = np.random.randn(i_size, layer_output_size)
+            b = np.zeros(layer_output_size) + 0.01
+            layers.append((W, b))
+
+            i_size = layer_output_size
+        return layers
+
+    def predict(self, inputs=None):
+        a = self.x_data if inputs is None else inputs
+        for (W, b), activation_func in zip(self.layers, self.activation_funcs):
+            z = np.dot(a, W) + b
+            a = activation_func(z)
+        return a
+
+    def cost(self):
+        predicts = self.predict()
+        return self.cost_fun(predicts, self.y_data)
+    
+    def _feed_forward_saver(self, inputs=None):
+        layer_inputs = []
+        zs = []
+        a = self.x_data if inputs is None else inputs
+        for (W, b), activation_func in zip(self.layers, self.activation_funcs):
+            layer_inputs.append(a)
+            z = np.dot(a, W) + b
+            a = activation_func(z)
+            zs.append(z)
+
+        return layer_inputs, zs, a
+
+    def backpropagation(self, inputs=None, targets=None):
+        inputs = self.x_data if inputs is None else inputs
+        targets = self.y_data if targets is None else targets
+
+        layer_inputs, zs, predicts = self._feed_forward_saver(inputs)
+        layer_grads = [() for _ in self.layers]
+
+        # We loop over the layers, from the last to the first
+        for i in reversed(range(len(self.layers))):
+            layer_input = layer_inputs[i]
+            z = zs[i]
+            activation_der = self.activation_ders[i]
+
+            if i == len(self.layers) - 1:
+                # For last layer we use cost derivative as dC_da(L) can be computed directly
+                dC_da = self.cost_der(predicts, targets)
+                # elementwise multiply by activation derivative
+                dC_dz = (activation_der(z) * dC_da.T).T
+            else:
+                # For other layers we build on previous z derivative: dC_da_prev = dC_dz @ W_next.T
+                next_W, _ = self.layers[i + 1]
+                dC_da = next_W @ dC_dz.T
+                dC_dz = activation_der(z) * dC_da.T
+
+            dC_dW = layer_input.T @ dC_dz
+            # return bias gradient as 1D array (matches stored b shape)
+            dC_db = np.mean(dC_dz, axis=0)
+
+            layer_grads[i] = (dC_dW, dC_db)
+
+        return layer_grads
+
+    def update_weights(self, layer_grads, learning_rate=0.001):
+        for idx, ((W, b), (W_g, b_g)) in enumerate(zip(self.layers, layer_grads)):
+            W = W - learning_rate * W_g
+            b = b - learning_rate * b_g
+            self.layers[idx] = (W, b)
+
+    def update_weights_RMSProp(self, layer_grads, G_iter, learning_rate=0.001, delta=1e-8, rho=0.9):
+        for idx, ((W, b), (W_g, b_g)) in enumerate(zip(self.layers, layer_grads)):
+            G_iter[idx] = (rho * G_iter[idx][0] + (1 - rho) * W_g ** 2, rho * G_iter[idx][1] + (1 - rho) * b_g ** 2)
+            W = W - learning_rate * W_g / (np.sqrt(G_iter[idx][0]) + delta)
+            b = b - learning_rate * b_g / (np.sqrt(G_iter[idx][1]) + delta)
+            self.layers[idx] = (W, b)
+
+    def update_weights_ADAM(self, layer_grads, first_moment, second_moment, i, learning_rate=0.001, delta=1e-8, beta1=0.9, beta2=0.999):
+        for idx, ((W, b), (W_g, b_g)) in enumerate(zip(self.layers, layer_grads)):
+            first_moment[idx] = (beta1 * first_moment[idx][0] + (1 - beta1) * W_g, beta1 * first_moment[idx][1] + (1 - beta1) * b_g)
+            second_moment[idx] = (beta2 * second_moment[idx][0] + (1 - beta2) * W_g ** 2, beta2 * second_moment[idx][1] + (1 - beta2) * b_g ** 2)
+            first_term_corr_W = first_moment[idx][0] / (1 - beta1 ** i)
+            first_term_corr_b = first_moment[idx][1] / (1 - beta1 ** i)
+            second_term_corr_W = second_moment[idx][0] / (1 - beta2 ** i)
+            second_term_corr_b = second_moment[idx][1] / (1 - beta2 ** i)
+            update_W = learning_rate * first_term_corr_W / (np.sqrt(second_term_corr_W) + delta)
+            update_b = learning_rate * first_term_corr_b / (np.sqrt(second_term_corr_b) + delta)
+            W = W - update_W
+            b = b - update_b
+            self.layers[idx] = (W, b)
+
+    def train_network_stochastic_gd(
+        self, learning_rate=0.001, epochs=100, momentum=0.3, minibatch_size=1
+    ):
+        change = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in self.layers ]
+        n_data = self.x_data.shape[0]
+        m = int(n_data / minibatch_size)
+        for epoch in range(epochs):
+            indices = np.random.permutation(n_data)
+            x_shuffled = self.x_data[indices]
+            y_shuffled = self.y_data[indices]
+            for batch_idx in range(m):
+                start = batch_idx * minibatch_size
+                end = start + minibatch_size
+                xi = x_shuffled[start:end]
+                yi = y_shuffled[start:end]
+                layer_grads = self.backpropagation(xi, yi)
+                for idx, ((W, b), (W_g, b_g), (W_c, b_c)) in enumerate(zip(self.layers, layer_grads, change)):
+                    new_change_W = learning_rate * W_g + momentum * W_c
+                    new_change_b = learning_rate * b_g + momentum * b_c
+                    W -= new_change_W
+                    b -= new_change_b
+                    self.layers[idx] = (W, b)
+                    change[idx] = (new_change_W, new_change_b) 
+    
+    def train_network_plain_gd(
+        self, learning_rate=0.01, max_iter=100000, stopping_criteria=1e-10, lr_method=None, delta=1e-8, rho=0.9, beta1=0.9, beta2=0.999
+    ):
+        if lr_method == "RMSProp":
+            G_iter = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in self.layers ]
+        elif lr_method == "ADAM":
+            first_moment = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in self.layers ]
+            second_moment = [ (np.zeros_like(W), np.zeros_like(b)) for (W, b) in self.layers ]
+        for i in range(max_iter):
+            i += 1
+            layer_grads = self.backpropagation()
+            prev_layers = self.layers.copy()
+            if lr_method == "RMSProp":
+                self.update_weights_RMSProp(layer_grads, G_iter, learning_rate, delta, rho)
+            elif lr_method == "ADAM":
+                self.update_weights_ADAM(layer_grads, first_moment, second_moment, i, learning_rate, delta, beta1, beta2)
+            else:
+                self.update_weights(layer_grads, learning_rate)
+            # Compute previous and current cost and stop if improvement below threshold
+            # prev_layers was the network state before the weight update
+            current_layers = self.layers
+            self.layers = prev_layers
+            prev_cost = self.cost()
+            self.layers = current_layers
+            curr_cost = self.cost()
+
+            if abs(prev_cost - curr_cost) <= stopping_criteria:
+                print(f"Early stopping at iteration {i}, cost change {abs(prev_cost - curr_cost):.2e} <= {stopping_criteria}")
+                break
